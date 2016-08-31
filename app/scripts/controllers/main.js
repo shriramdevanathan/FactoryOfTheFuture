8fb0c72f8caa6bf729b169d6a3838e01589f689f
shriramdevanathan <ghai12000@gmail.com>
2016-08-31 16:15:35 +0800

    Linked to tables page, removed table from sidebar

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,5 +7,116 @@
  * Controller of the sbAdminApp
  */
 angular.module('sbAdminApp')
-  .controller('MainCtrl', function($scope,$position) {
-  });
+    .controller('MainCtrl', function($scope, $position, $state) {
+
+        $scope.instrumentObjArr = [{
+            instrumentName: "QuantStudio Q3 & Q5",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "ViiA 7",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "7500 Fast",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "ViiA 7",
+            comments: "",
+            colour: "yellow",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "7500 Fast",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "Step One & Step One Plus",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "QuantStudio Q3 & Q5",
+            comments: "",
+            colour: "red",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "ViiA 7",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "7500 Fast",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "ViiA 7",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "7500 Fast",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "Step One & Step One Plus",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "ViiA 7",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "7500 Fast",
+            comments: "",
+            colour: "green",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }, {
+            instrumentName: "Step One & Step One Plus",
+            comments: "",
+            colour: "yellow",
+            linelead: "Shriram",
+            supervisor: "Michael",
+            type: "comments"
+        }];
+
+        $scope.clickInstrument = function(instr) {
+            $state.go('dashboard.table');
+        }
+    });
