f2f219f7a5fee77ee1874432a7c0aa3f71bdd05d
zodiax <mackyboy@gmail.com>
2016-10-06 11:34:15 +0800

    Update for changing port and host. Also remove the live reload

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,10 +35,10 @@ module.exports = function(grunt) {
             },
             js: {
                 files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-                tasks: ['newer:jshint:all'],
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                }
+                tasks: ['newer:jshint:all']
+                    /*options: {
+                        livereload: '<%= connect.options.livereload %>'
+                    }*/
             },
             jsTest: {
                 files: ['test/spec/{,*/}*.js'],
@@ -66,10 +66,10 @@ module.exports = function(grunt) {
         // The actual grunt server settings
         connect: {
             options: {
-                port: 9000,
+                port: 8080,
                 // Change this to '0.0.0.0' to access the server from outside.
-                hostname: '0.0.0.0',
-                livereload: 35729
+                hostname: 'localhost'
+                    //livereload: 35729
             },
             livereload: {
                 options: {
